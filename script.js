ad93e877e08218949db5e9b791bffb91a291ed78
Praful Parmar <prafulparmar095@gmail.com>
2025-06-17 13:53:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+const textBox = document.getElementById("evaluatedText");
+    const countDisplay = document.getElementById("letterCount");
+
+    textBox.addEventListener("input", () => {
+      countDisplay.textContent = textBox.value.length;
